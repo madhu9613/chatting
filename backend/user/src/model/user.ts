64641619a152d2760b31de4116a8625733f23529
mady9613 <rajkhowamadhujya0@gmail.com>
2026-04-29 20:47:12 +0530

    working on models for user microservices

---
+++ backend/user/src/model/user.ts
@@ -0,0 +1,22 @@
+import mongoose,{Document,Schema} from "mongoose";
+
+
+
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    avatar: string;
+    
+}
+const schema:Schema<IUser> = new Schema(
+    {
+        name: { type: String, required: true }, 
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        avatar: { type: String },
+    },{
+        timestamps: true,
+    }
+);
+export default mongoose.model<IUser>('User', schema);
