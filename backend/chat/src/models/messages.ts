19b50ef45031b273d9b0b92e33806cd6bf75a18d
mady9613 <rajkhowamadhujya0@gmail.com>
2026-04-30 20:52:40 +0530

    message model

---
+++ backend/chat/src/models/messages.ts
@@ -0,0 +1,59 @@
+import mongoose,{Document,Schema,Types} from "mongoose";
+
+export interface IMessage extends Document{
+    chatId: Types.ObjectId;
+    sender:string;
+    text?:string;
+    image?:{
+        url:string;
+        publicId:string;
+    };
+
+    messageType:"text" | "image";
+    seen:boolean;
+    seenAt?:Date;
+    createdAt:Date;
+    updatedAt:Date;
+}
+
+const schema=new Schema<IMessage>({
+    chatId:{
+        type:Schema.Types.ObjectId, 
+        ref:"Chat",
+        required:true
+    },
+    sender:{
+        type:String,
+        required:true
+    },
+    text:{
+        type:String,
+        trim:true
+    },
+    image:{
+        url:{
+            type:String,
+            trim:true
+        },
+        publicId:{
+            type:String,
+            trim:true
+        }
+    },
+    messageType:{
+        type:String,
+        enum:["text","image"],
+        defaiult:"text"
+    },
+    seen:{
+        type:Boolean,
+        default:false
+    },
+    seenAt:{
+        type:Date,
+        default:null
+    }
+},{timestamps:true});
+
+export const Messages=mongoose.model<IMessage>("Message",schema);
+
