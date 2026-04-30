6fe70b0b7f01f5edf6ab1d83f9123b8e85a05488
mady9613 <rajkhowamadhujya0@gmail.com>
2026-05-01 01:27:21 +0530

    fix cors issue

--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db';
 import chatRoutes from './routes/chat';
-
+import cors from 'cors';
 dotenv.config();
 const app=express()
 const port=process.env.PORT || 3000
@@ -10,6 +10,7 @@ const port=process.env.PORT || 3000
 connectDB();
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/v1', chatRoutes);
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
