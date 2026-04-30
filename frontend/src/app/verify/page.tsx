a3e3ddf2bf94054a30c75c3db6e907a9cb269438
mady9613 <rajkhowamadhujya0@gmail.com>
2026-05-01 01:32:46 +0530

    completed verify otp part

---
+++ frontend/src/app/verify/page.tsx
@@ -0,0 +1,14 @@
+// import Loading from "@/components/Loading";
+import Loading from "@/components/Loading";
+import VerifyOtp from "@/components/VerifyOtp";
+import { Suspense } from "react";
+
+const VerifyPage = () => {
+    return (
+        <Suspense fallback={<Loading />}>
+            <VerifyOtp />
+        </Suspense>
+    );
+};
+
+export default VerifyPage;
