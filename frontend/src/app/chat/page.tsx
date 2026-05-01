bb8e652aa872d2f7ec747f2915cf4353850f9cf3
mady9613 <rajkhowamadhujya0@gmail.com>
2026-05-01 16:38:48 +0530

    implemented ChatApp page

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,27 +1,319 @@
 "use client";
 
+import ChatSidebar from '@/components/ChatSidebar';
 import Loading from '@/components/Loading';
-import { useAppData } from '@/context/AppContext';
-import { redirect } from 'next/dist/client/components/navigation';
-import router from 'next/dist/shared/lib/router/router';
-import React, { useEffect } from 'react'
+import { useAppData, User, chat_service, user_service } from '@/context/AppContext';
+import { log } from 'console';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import { toast } from 'react-hot-toast';
+import ChatHeader from '@/components/ChatHeader';
+import ChatMessages from '@/components/ChatMessages';
+import MessageInput from '@/components/MessageInput';
 
-const page = () => {
-    const {loading,isAuth}=useAppData();
+export interface Message{
+    _id:string;
+    chatId:string;
+    sender:string;
+    text?:string;
+    image?:{
+        url:string;
+        publicId:string;
+    };
+    messageType:"text" | "image";
+    seen:boolean;
+    seenAt?:string;
+    createdAt:string;
+}
 
+
+
+const ChatApp = () => {
+    const router = useRouter();
+    const {loading,isAuth,logoutUser,chats,user:loggedInUser,users,fetchChats,setChats}=useAppData();
+   
+    const [selectedUser, setSelectedUser] = useState<string | null>(null);
+    const [message, setMessage] = useState("");
+    const [siderbarOpen, setSiderbarOpen] = useState(false);
+    const [messages, setMessages] = useState<Message[] | null>(null);
+    const [user, setUser] = useState<User | null>(null);
+    const [showAllUser, setShowAllUser] = useState(false);
+    const [isTyping, setIsTyping] = useState(false);
+    const [typingTimeOut, setTypingTimeOut] = useState<NodeJS.Timeout | null>(
+        null
+    );
+
+
+  
     useEffect(()=>{
         if(!isAuth && !loading){
-            redirect("/login");
+            router.push("/login");
         }   
-    },[isAuth,router,loading])
+    },[isAuth,loading,router])
 
-    if(loading){
-        return <Loading/>
+   
+
+    const handleLogout=async()=>{
+       logoutUser();
+    }
+    const moveChatToTop = (
+        chatId: string,
+        newMessage: any,
+        updatedUnseenCount = true
+    ) => {
+        setChats((prev) => {
+            if (!prev) return null;
+
+            const updatedChats = [...prev];
+            const chatIndex = updatedChats.findIndex(
+                (chat) => chat.chat._id === chatId
+            );
+
+            if (chatIndex !== -1) {
+                const [moveChat] = updatedChats.splice(chatIndex, 1);
+
+                const updatedChat = {
+                    ...moveChat,
+                    chat: {
+                        ...moveChat.chat,
+                        latestMessage: {
+                            text: newMessage.text,
+                            sender: newMessage.sender,
+                        },
+                        updatedAt: new Date().toString(),
+
+                        unseenCount:
+                            updatedUnseenCount && newMessage.sender !== loggedInUser?._id
+                                ? (moveChat.chat.unseenCount || 0) + 1
+                                : moveChat.chat.unseenCount || 0,
+                    },
+                };
+
+                updatedChats.unshift(updatedChat);
+            }
+
+            return updatedChats;
+        });
+    };
+
+    const resetUnseenCount = (chatId: string) => {
+        setChats((prev) => {
+            if (!prev) return null;
+
+            return prev.map((chat) => {
+                if (chat.chat._id === chatId) {
+                    return {
+                        ...chat,
+                        chat: {
+                            ...chat.chat,
+                            unseenCount: 0,
+                        },
+                    };
+                }
+                return chat;
+            });
+        });
+    };
+    async function fetchChat() {
+        const token = Cookies.get("token");
+        try {
+            const { data } = await axios.get(
+                `${chat_service}/send/message/${selectedUser}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+
+            setMessages(data.messages);
+            setUser(data.user);
+            await fetchChats();
+        } catch (error) {
+            console.log(error);
+            toast.error("Failed to load messages");
+        }
     }
 
+    const handleTyping = (value: string) => {
+        setMessage(value);
+
+        if (!selectedUser) return;
+
+        // // socket setup
+        // if (value.trim()) {
+        //     socket.emit("typing", {
+        //         chatId: selectedUser,
+        //         userId: loggedInUser?._id,
+        //     });
+        // }
+
+        // if (typingTimeOut) {
+        //     clearTimeout(typingTimeOut);
+        // }
+
+        // const timeout = setTimeout(() => {
+        //     socket.emit("stopTyping", {
+        //         chatId: selectedUser,
+        //         userId: loggedInUser?._id,
+        //     });
+        // }, 2000);
+
+        // setTypingTimeOut(timeout);
+    };
+    const handleMessageSend = async (e: any, imageFile?: File | null) => {
+        e.preventDefault();
+
+        if (!message.trim() && !imageFile) return;
+
+        if (!selectedUser) return;
+
+        // // socket work
+        // if (typingTimeOut) {
+        //     clearTimeout(typingTimeOut);
+        //     setTypingTimeOut(null);
+        // }
+
+        // socket?.emit("stopTyping", {
+        //     chatId: selectedUser,
+        //     userId: loggedInUser?._id,
+        // });
+
+        const token = Cookies.get("token");
+
+        try {
+            const formData = new FormData();
+
+            formData.append("chatId", selectedUser);
+
+            if (message.trim()) {
+                formData.append("text", message);
+            }
+
+            if (imageFile) {
+                formData.append("image", imageFile);
+            }
+
+            const { data } = await axios.post(
+                `${chat_service}/send/message`,
+                formData,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        "Content-Type": "multipart/form-data",
+                    },
+                }
+            );
+
+            setMessages((prev) => {
+                const currentMessages = prev || [];
+                const messageExists = currentMessages.some(
+                    (msg) => msg._id === data.message._id
+                );
+
+                if (!messageExists) {
+                    return [...currentMessages, data.message];
+                }
+                return currentMessages;
+            });
+
+            setMessage("");
+
+            const displayText = imageFile ? "📷 image" : message;
+
+            moveChatToTop(
+                selectedUser!,
+                {
+                    text: displayText,
+                    sender: data.sender,
+                },
+                false
+            );
+        } catch (error: any) {
+            toast.error(error.response.data.message);
+        }
+    };
+
+    
+
+
+    async function createChat(u: User) {
+        try {
+            const token = Cookies.get("token");
+            const { data } = await axios.post(
+                `${chat_service}/chat/new`,
+                {
+                    userId: loggedInUser?._id,
+                    otherUserId: u._id,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+
+            setSelectedUser(data.chatId);
+            setShowAllUser(false);
+            await fetchChats();
+        } catch (error) {
+            toast.error("Failed to start chat");
+        }
+    }
+
+    useEffect(()=>{
+        if(selectedUser){
+            fetchChat();
+        }
+    },[selectedUser])
+
+
+    if (loading) {
+        return <Loading />
+    }
   return (
-    <div>ChatApp</div>
+    <div className='min-h-screen flex bg-gray-900 text-white relative overflow-hidden'>
+        
+          <ChatSidebar
+              sidebarOpen={siderbarOpen}
+              setSidebarOpen={setSiderbarOpen}
+              showAllUsers={showAllUser}
+              setShowAllUsers={setShowAllUser}
+              users={users}
+              loggedInUser={loggedInUser}
+              chats={chats}
+              selectedUser={selectedUser}
+              setSelectedUser={setSelectedUser}
+              handleLogout={handleLogout}
+              createChat={createChat}
+            //   onlineUsers={onlineUsers}
+          />
+          <div className='flex-1 flex flex-col justify-between p-4 backdrop-blur-xl bg-white/5 border-1 border-white/10'>
+
+              <ChatHeader
+                  user={user}
+                  setSidebarOpen={setSiderbarOpen}
+                  isTyping={isTyping}
+                //   onlineUsers={onlineUsers}
+              />
+
+              <ChatMessages
+                  selectedUser={selectedUser}
+                  messages={messages}
+                  loggedInUser={loggedInUser}
+              />
+
+              <MessageInput
+                  selectedUser={selectedUser}
+                  message={message}
+                  setMessage={handleTyping}
+                  handleMessageSend={handleMessageSend}
+              />
+          </div>
+    </div>
   )
 }
 
-export default page
+export default ChatApp
